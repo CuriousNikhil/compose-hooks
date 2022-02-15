d455889e8773d56554a919292dbc9c40629a4fab
curiousNikhil <nikhyl777@gmail.com>
2022-02-15 15:28:57 +0530

    Add publishing scripts to useReducer hook

--- useReducer/build.gradle.kts
+++ useReducer/build.gradle.kts
@@ -4,6 +4,16 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
+val version = "1.0.0-alpha1"
+
+extra.apply {
+    set("PUBLISH_GROUP_ID", "me.nikhilchaudhari")
+    set("PUBLISH_VERSION", version)
+    set("PUBLISH_ARTIFACT_ID", "compose-usereducer")
+}
+
+apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
+
 android {
     buildFeatures {
         compose = true
